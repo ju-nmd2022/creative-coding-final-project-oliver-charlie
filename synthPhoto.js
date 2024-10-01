00efaf41f942ac17a13090aead0a248d8a9cfc00
Charlie Örneskog <charlie.orneskog@gmail.com>
2024-10-01 09:35:34 +0200

    Added centered video camera

--- synthPhoto.js
+++ synthPhoto.js
@@ -1,3 +1,17 @@
+let video;
+
 function setup() {
-    createCanvas(1000, 1000);
-  }
+  createCanvas(innerWidth, innerHeight); // Create a canvas that fits the window
+  video = createCapture(VIDEO);
+  video.size(640, 480); // Set the video size
+  video.hide(); // Hide the HTML video element
+}
+
+function draw() {
+  background(0);
+  // Calculate the x and y positions to center the video
+  let x = (width - video.width) / 2;
+  let y = (height - video.height) / 2;
+
+  image(video, x, y, 640, 480); // Draw the video at the centered position
+}
